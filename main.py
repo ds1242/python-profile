fbbe658145e7f21335a80b504fc3a3befe5f91f3
David Shaw <dshaw11@utk.edu>
2024-02-25 07:16:25 -0500

    imported header into all pages so header page and navbar render on all pages

--- main.py
+++ main.py
@@ -12,5 +12,17 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
