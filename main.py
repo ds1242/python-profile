fa47e13a669e431116c0f9c5555d6f98d5700425
David Shaw <dshaw11@utk.edu>
2024-02-25 07:05:42 -0500

    created html pages needed to render links for the navbar

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+Bootstrap5(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
